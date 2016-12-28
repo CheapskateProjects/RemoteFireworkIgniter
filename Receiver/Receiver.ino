f0097b55b2f882e5ff6d4e9f353dd3c2cde9fcc3
CheapskateProjects <cheapskateprojects@gmail.com>
2016-12-28 18:14:21 +0200

    Initial state

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -35,6 +35,9 @@ void setup(void)
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
 
   // Init radio
   radio.begin();
